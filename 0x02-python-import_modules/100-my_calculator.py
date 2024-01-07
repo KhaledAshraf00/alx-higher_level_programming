3221fce8fc01b6ce00654afaaafc71f67b6891ea
KhaledAshraf00 <khalidbedo2000@gmail.com>
2024-01-07 09:32:12 -0800

    Modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
+0;276;0cif __name__ == "__main__":
     import sys
 
     nargs = len(sys.argv) - 1
@@ -24,4 +24,3 @@ if __name__ == "__main__":
         print("{} * {} = {}".format(a, b, mul(a, b)))
     else:
         print("{} / {} = {}".format(a, b, div(a, b)))
-        
